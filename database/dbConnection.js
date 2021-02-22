3bc8fadd16d282b290ed73c83f30ed08f574975a
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2021-02-21 21:08:04 -0300

    change dbConnetion

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,22 +1,9 @@
-// const Sequelize = require('sequelize');
-// const connection = new Sequelize('db_krekBot', 'root', '25062001', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     logging: false,
-//     timezone: "-03:00"
-// });
-
-// module.exports = connection;
-
-const Sequelize = require("sequelize");
-const userHeroku = "babcd38cda6a7d";
-const passwordHeroku = "2fd727f1";
-const hostHeroku = "us-cdbr-east-03.cleardb.com";
-const herokuDatabase = "heroku_6b668b0e99dc0fa";
-const connection = new Sequelize(herokuDatabase, userHeroku, passwordHeroku, { //db_krekBot, root, senha
-    host: hostHeroku, // host - onde esta rodando o db -> localhost
-    dialect: 'mysql', // qual banco de dados estamos utilizando
-    logging: false
+const Sequelize = require('sequelize');
+const connection = new Sequelize('db_krekBot', 'root', '25062001', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false,
+    timezone: "-03:00"
 });
 
 module.exports = connection;
